2ad412c8d7d8ffab6c831020ad2161e5d5791149
Aaron Ma <aaronhma@gmail.com>
2019-05-19 20:37:18 -0700

    CREATE: validate

---
+++ src/brain/src/validate.h
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <atomic>
+#include <exception>
+#include <map>
+#include <memory>
+#include <set>
+#include <stdint.h>
+#include <string>
+#include <utility>
+#include <vector>
+
+class Apollo;
