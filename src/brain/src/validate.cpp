46dc4156dbf4d8e0e297059cb8bfa6efb3800d0b
Aaron Ma <aaronhma@gmail.com>
2019-05-19 20:59:35 -0700

    UPDATE: validate

--- src/brain/src/validate.cpp
+++ src/brain/src/validate.cpp
@@ -1,1 +1,11 @@
 #include <validate.h>
+#include <future>
+#include <sstream>
+#include <string>
+
+#include <boost/algorithm/string/replace.hpp>
+#include <boost/thread.hpp>
+
+#if defined(NDEBUG)
+# error "[RobotOS][ERROR] Expected assertions, but none found."
+#endif
