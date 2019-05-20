31610b3d873d8855feee485455d64b7edcc83c77
Aaron Ma <aaronhma@gmail.com>
2019-05-20 16:59:10 -0700

    UPDATE: apps

--- apps/Settings.app/main.py
+++ apps/Settings.app/main.py
@@ -1,11 +1,23 @@
-# from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout
-from PyQt5.QtWidgets import *
-app = QApplication([])
-window = QWidget()
-label = QLabel('Settings.app')
-label.show()
-layout = QVBoxLayout()
-layout.addWidget(QPushButton(''))
-window.setLayout(layout)
-window,show()
-app.exec_()
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'Settings'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        self.initUI()
+        
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.show()
+    
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
