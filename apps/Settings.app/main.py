cb91706a394a3cd0f5241f879ed8ca9cf2ce4fbe
Aaron Ma <aaronhma@gmail.com>
2019-05-20 17:04:31 -0700

    UPDATE: apps

--- apps/Settings.app/main.py
+++ apps/Settings.app/main.py
@@ -1,8 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QPushButton
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
 
-class App(QWidget):
+
+class App(QMainWindow):
     def __init__(self):
         super().__init__()
         self.title = 'Settings'
@@ -10,13 +12,35 @@ class App(QWidget):
         self.top = 10
         self.width = 640
         self.height = 480
-        self.initUI()
-        
-    def initUI(self):
+        self.window()
+        self.menu()
+
+    def window(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.show()
-    
+
+    def menu(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('File')
+        editMenu = mainMenu.addMenu('Edit')
+        viewMenu = mainMenu.addMenu('View')
+        searchMenu = mainMenu.addMenu('Search')
+        toolsMenu = mainMenu.addMenu('Tools')
+        helpMenu = mainMenu.addMenu('Help')
+
+        exitButton = QAction(QIcon('exit24.png'), 'Exit', self)
+        exitButton.setShortcut('Ctrl+Q')
+        exitButton.setStatusTip('Exit application')
+        exitButton.triggered.connect(self.close)
+        fileMenu.addAction(exitButton)
+
+        self.show()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
